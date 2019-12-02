733dd1701ebcce22bfeac08100af4f402a559611
Yoshihiro Tanaka <contact@cordea.jp>
2019-12-02 22:03:50 +0900

    add deploy script

---
+++ scripts/codelab_gen.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# encoding:utf-8
+
+readonly base_dir="/tmp/mps_codelab"
+readonly file="$base_dir/$(date +%s).md"
+
+mkdir $base_dir
+
+cp md/chapters/*.png "$base_dir/."
+
+cat md/INDEX.md > $file
+cat md/chapters/*.md >> $file
+/tmp/claat export -o docs/ $file
+
+rm -r $base_dir
---
+++ scripts/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# encoding:utf-8
+
+./scripts/download_claat.sh
+./scripts/codelab_gen.sh
